cba11e8771d74253c3228d682b967906d8639092
Jesse Woolsey <jesse232@live.com>
2020-01-13 10:22:25 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,5 +9,5 @@ def display_board(board)
   puts "#{board[6]} |  #{board[7]}  | #{board[8]}"
 end
 
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = ["X", "X", "O", "O", "X", "X", "O", "O", "X"]
 display_board(board)
