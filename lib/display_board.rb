3803eb855f4dd771c1ce2fcaf35bc76707485758
Jesse Woolsey <jesse232@live.com>
2020-01-13 10:17:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
-  puts "  #{board[6]} |  #{board[7]}  |  #{board[8]} "
+  puts "   |   |   "
 end
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
